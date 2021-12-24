315dcc8463c6903e6738b43326a4fb1be6962ed4
Krissy Heishman <krissy@heishman.dev>
2021-12-23 22:54:59 -0600

    reflect naming change

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(
-    name='tljh-odc',
+    name='tljh-opendatacube',
     version='0.1',
     description='An Open Data Cube (ODC) deployment of The Littlest JupyterHub (TLJH)',
-    url='https://github.com/AMA-Labs/odc-tljh',
+    url='https://github.com/AMA-Labs/tljh-opendatacube',
     author='AMA Earth Analytics Lab',
     author_email='eal@ama-inc.com',
     license='Apache License 2.0',
     entry_points={
         'tljh': [
-            'odc = tljh_odc',
+            'odc = tljh_opendatacube',
         ]
     },
-    py_modules = ['tljh_odc'],
+    py_modules = ['tljh_opendatacube'],
     install_requires=['sh']
 )
